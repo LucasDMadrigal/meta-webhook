2661fbebbd8fce3ee346e0b9a8273914a1b5d7a4
LucasDMadrigal <63689668+LucasDMadrigal@users.noreply.github.com>
2025-09-11 13:14:17 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const verifyToken = process.env.WEBHOOKVTOKEN;
 // Route for GET requests
 app.get('/', (req, res) => {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
-
+    console.log(req.query)
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
